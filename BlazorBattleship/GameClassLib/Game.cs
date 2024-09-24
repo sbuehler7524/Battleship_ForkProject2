a807a1a69c21d794813b52e3ea37e0a6aeefe07e
Samuel Buehler <samuel@samueldb.com>
2024-09-24 12:35:17 -0500

    Update Game.cs

--- BlazorBattleship/GameClassLib/Game.cs
+++ BlazorBattleship/GameClassLib/Game.cs
@@ -11,39 +11,37 @@
             Console.WriteLine("Choose the number of ships you want to play with: ");
             // Read the user's input and convert it to an integer representing the number of ships.
             int shipNum = Convert.ToInt32(Console.ReadLine());
+            // Check if player is playing against a computer
+            bool playVsComp = false;
             // Create two Player instances with the specified number of ships and assign names.
             var player1 = new Player(shipNum, "Player 1");
-            var player2 = new Player(shipNum, "Player 2");
-
-            bool playVsComp = false;
+            var player2;
+            if (!playVsComp) // If human opponent
+            {
+                player2 = new Player(shipNum, "Player 2"); // Assign other player
+            }
+            else // Else, comp opponent
+            {
+                player2 = new CompOpponent(shipNum, "COMP"); // Assign computer opponent
+            }
             
             // Start the main game loop.
-            if (!playVsComp) // If not playing against a computer...
+            while (true)
             {
-                while (true)
+                // Player 1 takes a turn attacking Player 2.
+                player1.TakeTurn(player2);
+                // Check if Player 2 has no ships (life) remaining.
+                if (player2.life == 0)
                 {
-                    // Player 1 takes a turn attacking Player 2.
-                    player1.TakeTurn(player2);
-                    // Check if Player 2 has no ships (life) remaining.
-                    if (player2.life == 0)
-                    {
-                        Console.WriteLine("Player 1 wins!");
-                        break; // Exit the game loop.
-                    }
-                    player2.TakeTurn(player1);
-                    // Check if Player 1 has no ships (life) remaining.
-                    if (player1.life == 0)
-                    {
-                        Console.WriteLine("Player 2 wins!");
-                        break;
-                    }
+                    Console.WriteLine("Player 1 wins!");
+                    break; // Exit the game loop.
                 }
-            }
-            else // Playing against a computer...
-            {
-                while (true)
+                player2.TakeTurn(player1);
+                // Check if Player 1 has no ships (life) remaining.
+                if (player1.life == 0)
                 {
-                    
+                    Console.WriteLine("Player 2 wins!");
+                    break;
                 }
             }
         }
